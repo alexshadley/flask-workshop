4eba32a3807a04a47c4c2bf63054c30e67e504db
Alex Shadley <shadleyalex@gmail.com>
2019-10-21 00:19:27 -0500

    checkpoint 3

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from trivia import get_questions
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    questions = get_questions(5)
+    return render_template("quiz.html", questions=questions)
 
 
 @app.route('/greet/<name>')
